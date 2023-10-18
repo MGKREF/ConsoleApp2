6c268fd74014690ed0c87242eb17581507901e75
mgkref <mgkref@gmail.com>
2023-10-18 10:13:52 +0330

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿class program
+{
+    class product
+
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int PRICE { get; set; }
+
+    }
+    static void Main()
+    {
+        product product = new product();
+    }
+}
