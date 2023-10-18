789f9d61d0a73df7d24c3cc0072b19f9c1e256c9
mgkref <mgkref@gmail.com>
2023-10-18 10:18:09 +0330

    payam

--- ConsoleApp2/product.cs
+++ ConsoleApp2/product.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp2
     internal class product
     {
         public int id { get; set; }
-        public string name { get; set; }
+        public string? name { get; set; }
 
     }
 }
